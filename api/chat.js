fdb525c51851b01147b08bbb4e7d53f8234b832e
An777-rep <a9304197@gmail.com>
2025-07-09 13:28:20 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": Bearer ${apiKey},
+        "Authorization": `Bearer ${apiKey}`,
         "HTTP-Referer": "https://travkagpt.vercel.app",
         "X-Title": "TravkaGPT"
       },
